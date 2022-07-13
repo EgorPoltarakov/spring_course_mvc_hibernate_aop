cbc317c786367e919abb2444eab6c767ac77164c
Poltarakov <epoltarakov.99@mail.ru>
2022-07-13 20:17:42 +0300

    Add CRUD operation for employee

--- src/main/java/com/egorpoltarakov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
+++ src/main/java/com/egorpoltarakov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -5,7 +5,12 @@ import com.egorpoltarakov.spring.mvc_hibernate_aop.entity.Employee;
 import java.util.List;
 
 public interface EmployeeDAO {
+
     public List<Employee> getAllEmployees();
 
-   public void saveEmployee(Employee employee);
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
 }
--- src/main/java/com/egorpoltarakov/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
+++ src/main/java/com/egorpoltarakov/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -35,7 +35,25 @@ public class EmployeeDAOImpl implements EmployeeDAO{
 
         Session session = sessionFactory.getCurrentSession();
 
-        session.save(employee);
+        session.saveOrUpdate(employee);
 
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Employee employee = session.get(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Query<Employee> query = session.createQuery("delete from Employee " +
+                "where id =: employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
     }
 }
